2c0c93f61434083c18b7424be2dc4f607f6ddf54
Velisarios1234 <fafoutis@csd.auth.gr>
2022-02-03 15:43:06 +0200

    working on the change directory command

--- FilesHelper.py
+++ FilesHelper.py
@@ -5,3 +5,14 @@ def loadCommandsList(path):
     for line in lines:
         tokenizedLines.append(line.rstrip())
     return tokenizedLines
+
+
+def constructPreviousPathFromList(pathList):
+    delimiter = "\\"
+    path = ""
+    for i in range(0, len(pathList) - 1):
+        if(i != len(pathList) - 1):
+           path = path + pathList[i] + delimiter
+        else:
+           path = path + pathList[i]
+    return path
--- customSubprocess.py
+++ customSubprocess.py
@@ -1,10 +1,44 @@
 from typing import Optional
 import sys
 import typer 
+import os
 import FilesHelper
 app = typer.Typer()
 
 
+@app.command()
+def chd(path : str):
+    currentDir = os.getcwd()
+    if(path == ".."):
+        cdlist = os.getcwd().split("\\")
+        path = FilesHelper.constructPreviousPathFromList(cdlist)
+        print(f"The path is :  {path}")
+        os.chdir(path)
+        print(os.getcwd())
+
+        
+
+
+
+
+## method to list the files in directory
+@app.command()
+def lD(path : Optional[str] = typer.Argument(None)):
+    dir_list = []
+    if(path is None):
+        dir_list = os.listdir(".")
+    else:
+        dir_list = os.listdir(path)
+    for dir in dir_list:
+           print(dir)
+  
+
+@app.command()
+def q():
+    typer.Abort()
+
+
+##--------  Example commands ---------##
 @app.command()
 def hello(name: Optional[str] = typer.Argument(None)):
      if not(name is None):
@@ -17,9 +51,6 @@ def goodbye(name: Optional[str] = typer.Argument(None)):
         typer.echo(f"Goodbye {name}")
 
         
-@app.command()
-def q():
-    typer.Abort()
 
     
 
